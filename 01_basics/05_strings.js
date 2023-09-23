ef1351cd8bdc2d34948e79be4eb11049df85401a
jaskaran-coder <74381596+jaskaran-coder@users.noreply.github.com>
2023-09-23 12:48:17 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,16 @@
+let name = "jaskaran"
+let repos = 50
+
+// console.log(name + repos + "Values");
+
+console.log(`My name is ${name} and my repo count is ${repos}`);
+
+const gameName = new String("jaskhs")
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+
+const url = "jaskaran singh.com"
+console.log(url.replace(' ', '-'));
